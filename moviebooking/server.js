34e1787057c596ef1d440eb3913ba447e673f05c
Durgesh <pandeydurgesh011@gmail.com>
2021-07-21 21:01:31 +0530

    CheckPoint 6

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -1,34 +1,11 @@
 const http = require("http");
-
-const db = require("./models");
-const PORT = 9000;
+const app = require("./app");
 
 
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+const PORT = process.env.PORT || 3000;
 
-const ROUTE_RESPONSE_MAP = {
-  "/movies": "All Movies Data in JSON format from Mongo DB",
-  "/genres": "All Genres Data in JSON format from Mongo DB",
-  "/artists": "All Artists Data in JSON format from Mongo DB",
-};
-
-const server = http.createServer((req, res) => {
-  if (ROUTE_RESPONSE_MAP[req.url]) {
-    res.end(ROUTE_RESPONSE_MAP[req.url]);
-  }
-});
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`server listening on port 9000`);
+  console.log(`server listening on port ${PORT}`);
 });
