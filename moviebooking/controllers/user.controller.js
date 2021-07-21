174c66dba69fcf8a1e2d994944e55f0b9fb0a00d
Durgesh <pandeydurgesh011@gmail.com>
2021-07-21 21:19:09 +0530

    CheckPoint 8

--- moviebooking/controllers/user.controller.js
+++ moviebooking/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const { User } = require("../models");
-
 const TokenGenerator = require("uuid-token-generator");
 const { uuid } = require("uuidv4");
 const { atob, btoa } = require("b2a");
@@ -35,7 +34,6 @@ async function signUp(req, res) {
 }
 
 async function login(req, res) {
-  //decrypt username and password by seperating the basic word
   const userNameAndPassword = atob(req.headers["Authorization"]).split(" ")[1];
   const { username, password } = userNameAndPassword.split(":");
   try {
@@ -76,8 +74,49 @@ async function logout(req, res) {
     });
 }
 
+async function getCouponCode(req, res) {
+  const accesstoken = req.header["Authorization"].split(" ")[1];
+  if (!uuid) {
+    return res.status(401).send("user not logged in");
+  }
+  try {
+    const users = await User.find({ accesstoken: accesstoken });
+    if (users[0].coupens) {
+      res.send(users[0].coupens);
+    } else {
+      res.send([]);
+    }
+  } catch (err) {
+    return res.status(500).send(err.message || "user not found");
+  }
+}
+
+async function bookShow(req, res) {
+  try {
+    const accessToken = req.header["Authorization"].split(" ")[1];
+    if (!accessToken) throw new Error("user not logged in");
+    const uuid = req.body.uuid;
+    const bookingRequest = req.body.bookingRequest;
+    User.findOneAndUpdate(
+      { uuid: uuid },
+      { $push: { bookingRequests: bookingRequest } }
+    )
+      .then(data => {
+        if (!data) throw new Error("unable to book show");
+        res.status(200).send(bookingRequest);
+      })
+      .catch(err => {
+        res.status(500).send(err.message || "unable to book show");
+      });
+  } catch (err) {
+    res.status(500).send(err.message || "unable to book show");
+  }
+}
+
 module.exports = {
   signUp,
   login,
   logout,
+  getCouponCode,
+  bookShow,
 };
